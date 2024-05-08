e81803eae5f4675ebdb36dcafdbc2a3079a4303d
h03einj <h03eiinj@gmail.com>
2024-05-08 09:43:23 +0330

    c34

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,25 @@
-﻿int a = int.Parse(Console.ReadLine());       
+﻿//sum
+int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 sum(a, b);
 static void sum(int a, int b)
 {
-    Console.WriteLine(a+b);
+    Console.WriteLine(a + b);
+    Console.WriteLine("hosssdeinjalali");
+    Console.WriteLine("hosssdeinjalali");
     Console.WriteLine("hosssdeinjalali");
     Console.WriteLine("hosssdeinjalali");
     Console.WriteLine("hosssdeinjalali");
 
+}
+multi(a, b);
+static void multi(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
+
+//Student Class
+class Student
+{
+    public int name { get; set; }
 }
