6083c1b4f95d9c33453f13dd8ae9f2d72296ba8c
mgkref <mgkref@gmail.com>
2024-05-08 10:27:15 +0330

    hack

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,15 +5,19 @@ sum(a, b);
 static void sum(int a, int b)
 {
     Console.WriteLine(a + b);
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine("hosssdeinjalali");
-    Console.WriteLine();
+
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
+    Console.WriteLine("system is hacking bad");
 }
+
 multi(a, b);
 static void multi(int a, int b)
 {
